800d983f47222045f2fe2bf9aaa4289fadd10f67
zachlamont <zachlamont1@gmail.com>
2023-10-04 21:56:11 +1000

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// Determine base dynamically based on environment
+const base =
+  process.env.NODE_ENV === "production"
+    ? "/pressure-calculator/"
+    : "/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/pressure-calculator/",
+});
